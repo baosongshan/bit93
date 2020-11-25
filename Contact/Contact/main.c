1207c05f9fb6b50683eac4a303509a4e9dc89af3
baosongshan <baosongshan2006@163.com>
2020-11-25 21:53:54 +0800

    add struct enum union

---
+++ Contact/Contact/main.c
@@ -0,0 +1,49 @@
+#include"contact.h"
+
+int main(int argc, char *argv[])
+{
+	Contact_t cont;
+	ContactInit(&cont);
+
+	int select = 1;
+	while(select)
+	{
+		printf("****************************************\n");
+		printf("* [1] AddContact   [2] DelContact      *\n");
+		printf("* [3] FindContact  [4] ShowContact     *\n");
+		printf("* [5] ModContact   [6] SortContact     *\n");
+		printf("* [7] ClearContact [8] DestroyContact  *\n");
+		printf("* [0] Exit System                      *\n");
+		printf("****************************************\n");
+		printf("ÇëÑ¡Ôñ:>");
+		scanf("%d", &select);
+
+		if(select == QUIT)
+			break;
+
+		switch(select)
+		{
+		case ADD:
+			break;
+		case DEL:
+			break;
+		case FIND:
+			break;
+		case SHOW:
+			break;
+		case MOD:
+			break;
+		case SORT:
+			break;
+		case CLEAR:
+			break;
+		case DESTROY:
+			break;
+		default:
+			break;
+
+		}
+	}
+
+	return 0;
+}
